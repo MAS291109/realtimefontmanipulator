afb0881a8ffe252fd80a6f5dc9852a4811592711
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-02-08 16:30:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,15 +15,29 @@ function setup(){
 
 function draw(){
     background('#abdeeb');
+    document.getElementById("font_size").innerHTML = "Font Size = "+difference+"px";
+    textSize(difference);
+    fill("#000000");
+    text('Meghna',70,200);
 }
 
 function modelLoaded(){
     console.log("PoseNet Is Working");
 }
 
-function gotPoses(results){
-    if(results.length > 0)
-    {
-        console.log(results);
+function gotPoses(results,error){
+    if(error){
+        console.log(error);
     }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_X = "+results[0].pose.rightWrist.x);
+        console.log("leftWrist_X = "+results[0].pose.leftWrist.x);
+    } 
 }
