19f635d7cc29fc1a14aaa0755f9b0647db49ae5f
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-02-08 18:35:33 +0530

    Add files via upload
    UI completed, JS started.

--- main.js
+++ main.js
@@ -1,43 +1,18 @@
-leftWrist_x = 0;
-rightWrist_x = 0;
-difference = 0;
+song = "";
 
+function preload(){
+    song = loadSound("music.mp3");
+}
 function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
     video = createCapture(VIDEO);
-    video.size(550, 500);
-
-    canvas = createCanvas(550, 500);
-    canvas.position(560,110);
-
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    video.hide();
 }
-
 function draw(){
-    background('#abdeeb');
-    document.getElementById("font_size").innerHTML = "Font Size = "+difference+"px";
-    textSize(difference);
-    fill("#000000");
-    text('Meghna',70,200);
+    image(video,0,0,600,500);
 }
-
-function modelLoaded(){
-    console.log("PoseNet Is Working");
-}
-
-function gotPoses(results,error){
-    if(error){
-        console.log(error);
-    }
-    if(results.length > 0){
-        console.log(results);
-
-        leftWrist_x = results[0].pose.leftWrist.x;
-        rightWrist_x = results[0].pose.rightWrist.x;
-
-        difference = floor(leftWrist_x - rightWrist_x);
-
-        console.log("rightWrist_X = "+results[0].pose.rightWrist.x);
-        console.log("leftWrist_X = "+results[0].pose.leftWrist.x);
-    } 
+function play(){
+    song.play();
 }
